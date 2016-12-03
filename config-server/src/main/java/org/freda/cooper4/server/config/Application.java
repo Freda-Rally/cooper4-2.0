fb3f45b38a7357eb77750bd8d4e621a5ebaeae6b
Rally <zhangyub@xiaojiuwo.cn>
2016-12-03 20:03:52 +0800

    [EDIT]根据实际应用情况..应先启动config-server.配置文件放在云端
    1.配置文件
    2.将config-server也变成eureka中的服务..

--- config-server/src/main/java/org/freda/cooper4/server/config/Application.java
+++ config-server/src/main/java/org/freda/cooper4/server/config/Application.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Config-Server 服务入口
@@ -12,7 +11,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * Created by rally on 2016/11/11.
  */
 @EnableConfigServer
-@EnableEurekaClient
 @SpringBootApplication
 public class Application
 {
